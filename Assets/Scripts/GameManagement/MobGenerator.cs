db819b1b271f778430dd9e5f94e76509b4407570
Enzo Berry <enzoberry69@gmail.com>
2023-03-06 12:22:04 +0100

    Mob generator optimised

--- Assets/Scripts/GameManagement/MobGenerator.cs
+++ Assets/Scripts/GameManagement/MobGenerator.cs
@@ -8,47 +8,73 @@ using Photon.Pun;
 using Photon.Realtime;
 
 using Random = UnityEngine.Random;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RogueCaml{
-public class MobGenerator : MonoBehaviour
-{
-    public float difficulty = 25f;
-    public BoxCollider2D bc;
-    private bool notTriggered = true;
-
-    public GameObject ennemyPrefab;
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class MobGenerator : MonoBehaviour
     {
-        
-    }
+        //public float difficulty = 25f;
+        public BoxCollider2D bc;
+        //private bool notTriggered = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public GameObject ennemyPrefab;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Debug.Log("trigger enter", this);
-        if(PhotonNetwork.IsMasterClient)
+        float timer = 0;
+
+        //Spawner
+        public int ennemies_per_round = 5;
+        public int delta_between_rounds = 8;
+
+        // Update is called once per frame
+        void Update()
         {
-
-            if(collision.gameObject.CompareTag("ally"))
+            if (timer < delta_between_rounds)
             {
-                notTriggered = false;
-                Vector2 size = bc.size;
-
-                for(int i = 0; i < 10; i++)
-                {
-                    PhotonNetwork.Instantiate(this.ennemyPrefab.name, transform.position + new Vector3( Random.Range(-size.x/2, size.x/2 ), Random.Range(-size.y/2, size.y/2), 0), Quaternion.identity, 0);
-                }
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                SpawnVague();
+                timer = 0;
             }
         }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SpawnVague();
+        }
+
+        //private void OnTriggerEnter2D(Collider2D collision)
+        //{
+        //    //Debug.Log("trigger enter", this);
+        //    if(PhotonNetwork.IsMasterClient)
+        //    {
+        //        if(collision.gameObject.CompareTag("ally"))
+        //        {
+        //            notTriggered = false;
+        //            Vector2 size = bc.size;
+
+        //            for(int i = 0; i < 10; i++)
+        //            {
+        //                PhotonNetwork.Instantiate(this.ennemyPrefab.name, transform.position + new Vector3( Random.Range(-size.x/2, size.x/2 ), Random.Range(-size.y/2, size.y/2), 0), Quaternion.identity, 0);
+        //            }
+        //        }
+        //    }
+        //}
+
+        private void SpawnVague()
+        {
+            Vector2 size = bc.size;
+
+            for (int j = 0; j < ennemies_per_round; j++)
+            {
+                PhotonNetwork.Instantiate(this.ennemyPrefab.name, transform.position + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), 0), Quaternion.identity, 0);
+            }
+
+        }
+        
     }
-}
 }
