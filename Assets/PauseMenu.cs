84a85f946cbc194fe14eb0d0bbf5d95e4961803f
maxime-del <84310481+maxime-del@users.noreply.github.com>
2023-03-07 19:49:30 +0100

    PauseMenu update
    Ajout d'un menu pause accessible via echap, reste a implementer le retour menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool is_paused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (is_paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Paused()
+    {
+        //avec arret 
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        is_paused = true;
+        
+    }
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        is_paused = false;
+    }
+}
